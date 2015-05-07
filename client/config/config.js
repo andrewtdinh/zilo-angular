4814d336056bec55b6f40a08c40e4cffbc78459a
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-07 13:44:05 -0700

    able to add a listing without price and other params

--- client/config/config.js
+++ client/config/config.js
@@ -9,7 +9,7 @@ angular.module('zilo')
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   // .state('profile', {url: '/profile', templateUrl: '/views/users/profile.html', controller: 'ProfileCtrl'})
-  .state('listings', {url: '/areas', templateUrl: '/views/listings/listings.html', abstract: true})
+  .state('listings', {url: '/listings', templateUrl: '/views/listings/listings.html', abstract: true})
   .state('listings.new', {url: '/new', templateUrl: '/views/listings/listings-new.html', controller: 'ListingsNewCtrl'})
   .state('listings.list', {url: '/', templateUrl: '/views/listings/listings-list.html', controller: 'ListingsListCtrl'})
   .state('listings.show', {url: '/{listingId}', templateUrl: '/views/listings/listings-show.html', controller: 'ListingsShowCtrl'});
