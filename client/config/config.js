9ed4cdd90ff5d2905b68a961c40706bd02329cfc
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-06 22:20:31 -0700

    update config.js

--- client/config/config.js
+++ client/config/config.js
@@ -6,7 +6,11 @@ angular.module('zilo')
 
   $stateProvider
   .state('home', {url: '/', templateUrl: '/views/general/home.html'})
-  .state('about', {url: '/about', templateUrl: '/views/general/about.html'})
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
-  .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'});
+  .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
+  .state('profile', {url: '/profile', templateUrl: '/views/users/profile.html', controller: 'ProfileCtrl'})
+  .state('areas', {url: '/areas', templateUrl: '/views/areas/areas.html', abstract: true})
+  .state('areas.new', {url: '/new', templateUrl: '/views/areas/areas-new.html', controller: 'AreasNewCtrl'})
+  .state('areas.list', {url: '/', templateUrl: '/views/areas/areas-list.html', controller: 'AreasListCtrl'})
+  .state('areas.show', {url: '/{areaId}', templateUrl: '/views/areas/areas-show.html', controller: 'AreasShowCtrl'});
 });
