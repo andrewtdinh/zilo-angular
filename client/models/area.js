e60c2e2e56c35f0e22f6e737b022c33e2dbe2b3f
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-07 00:23:38 -0700

    Add area model and area view

---
+++ client/models/area.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('roadtrip')
+.factory('Area', function($http, nodeUrl){
+
+  function Area(obj){
+    // this._id = obj._id;
+    this.name = obj.name;
+    // this.departure = obj.departure;
+  }
+
+  // Trip.show = function(tripId){
+  //   return $http.get(nodeUrl + '/trips/' + tripId);
+  // };
+  //
+  // Trip.find = function(){
+  //   return $http.get(nodeUrl + '/trips');
+  // };
+
+  Trip.prototype.save = function(){
+    return $http.post(nodeUrl + '/trips', this);
+  };
+
+  // Trip.prototype.destroy = function(){
+  //   return $http.delete(nodeUrl + '/trips/' + this._id);
+  // };
+  //
+  // Trip.prototype.destroyStop = function(stop){
+  //   return $http.delete(nodeUrl + '/trips/' + this._id + '/stops/' + stop._id);
+  // };
+  //
+  // Trip.prototype.addStop = function(stop){
+  //   return $http.post(nodeUrl + '/trips/' + this._id + '/stops', stop);
+  // };
+
+  return Trip;
+});
